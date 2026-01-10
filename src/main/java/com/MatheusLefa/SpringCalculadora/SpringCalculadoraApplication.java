1f73dee0a200783f9ae4add7c7409300c47cccb0
Matheus-Leffa <matheusleffa@live.com>
2026-01-10 10:53:28 -0300

    Primeiro commit: Estrutura de pastas

---
+++ src/main/java/com/MatheusLefa/SpringCalculadora/SpringCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.MatheusLefa.SpringCalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCalculadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MatheusLefa/SpringCalculadora/SpringCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MatheusLefa.SpringCalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
