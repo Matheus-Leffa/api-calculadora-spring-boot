a0fb6f8805847e915325b5abf6b403c32b459821
Matheus-Leffa <matheusleffa@live.com>
2026-01-21 19:01:50 -0300

    Correções no meu ExceptionHandler

--- src/main/java/com/MatheusLefa/SpringCalculadora/infra/GlobalExceptionHandler.java
+++ src/main/java/com/MatheusLefa/SpringCalculadora/infra/GlobalExceptionHandler.java
@@ -3,11 +3,14 @@ package com.MatheusLefa.SpringCalculadora.infra;
 import com.MatheusLefa.SpringCalculadora.exception.DivisaoPorZeroException;
 import com.MatheusLefa.SpringCalculadora.exception.EmailJaCadastradoException;
 import com.MatheusLefa.SpringCalculadora.exception.RecursoNaoEncontradoException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
@@ -30,20 +33,26 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .body(ex.getMessage());
     }
 
+    @ExceptionHandler(RecursoNaoEncontradoException.class)
     public ResponseEntity<String> handleRecursoNaoEncontrado(RecursoNaoEncontradoException ex){
         return ResponseEntity
                 .status((HttpStatus.NOT_FOUND))
                 .body(ex.getMessage());
     }
 
+    @ExceptionHandler(EmailJaCadastradoException.class)
     public ResponseEntity<String> handleEmailJaCadastrado(EmailJaCadastradoException ex){
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ex.getMessage());
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidacao(MethodArgumentNotValidException ex) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
 
         Map<String, String> erros = new HashMap<>();
 
