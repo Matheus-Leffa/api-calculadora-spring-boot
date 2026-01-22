6f5f020920abf88f868fa42820e7c59805862b7b
Matheus-Leffa <matheusleffa@live.com>
2026-01-22 19:27:06 -0300

    Implementando Authorization Service

--- src/main/java/com/MatheusLefa/SpringCalculadora/repository/UsuarioRepository.java
+++ src/main/java/com/MatheusLefa/SpringCalculadora/repository/UsuarioRepository.java
@@ -2,10 +2,11 @@ package com.MatheusLefa.SpringCalculadora.repository;
 
 import com.MatheusLefa.SpringCalculadora.domain.Entity.user.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    
+   UserDetails findByLogin(String login);
 
     boolean existsByEmail(String email);
 }
---
+++ src/main/java/com/MatheusLefa/SpringCalculadora/service/AuthorizationService.java
@@ -0,0 +1,23 @@
+package com.MatheusLefa.SpringCalculadora.service;
+
+import com.MatheusLefa.SpringCalculadora.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AuthorizationService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(username);
+    }
+}
